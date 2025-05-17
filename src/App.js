78b9963b3bd472f8e0e81f8020107648d9aaf9a9
Divya Akarsh Pathak <akarshpathak47@gmail.com>
2025-05-17 14:12:37 +0530

    Navigation change

--- src/App.js
+++ src/App.js
@@ -1,38 +1,25 @@
 
 import './App.css';
 import React from 'react';
-import blood from './img/blood.png';
+
+import { Routes,Route} from 'react-router-dom';  
+import Home from './components/home/home.js';
+import Second from './components/home/Second.js';
 
 
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Ayuudhara</h1>
-        <p>Your one-stop solution for all your needs.</p>
-        <img src={blood} alt="Blood Donation" className='blood' width={400} height={380}></img>
-        <button className="Next">Next</button>
-       
-       
-
-        
-      </header>
-      <main>
-        <h2>Features</h2>
-        <ul>
-          <li>Feature 1</li>
-          <li>Feature 2</li>
-          <li>Feature 3</li>
-        </ul>
-      </main>
-      <footer>
-        <p>&copy; 2025 Ayuudhara. All rights reserved.</p>
-      </footer>
-
-         
+       <Routes>
+  <Route path="/"  element={<Home/>} />
+  <Route path="/Second" element={<Second/>} />
+       </Routes>
     </div>
+
   );
+    
 }
 
 export default App;
---
+++ src/components/home/home.js
@@ -0,0 +1,39 @@
+import './home.css';
+import React from 'react';
+import blood from '../../img/blood.png';
+import{useNavigate} from 'react-router-dom';
+
+
+
+function Home() {
+    const navigate = useNavigate();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Ayuudhara</h1>
+        <p>Your one-stop solution for all your needs.</p>
+        <img src={blood} alt="Blood Donation" className='blood' width={400} height={380}></img>
+        <button className="Next" onClick={()=>navigate("/Second")}>Next</button>
+       
+       
+
+        
+      </header>
+      <main>
+        <h2>Features</h2>
+        <ul>
+          <li>Feature 1</li>
+          <li>Feature 2</li>
+          <li>Feature 3</li>
+        </ul>
+      </main>
+      <footer>
+        <p>&copy; 2025 Ayuudhara. All rights reserved.</p>
+      </footer>
+
+         
+    </div>
+  );
+}
+
+export default Home;
